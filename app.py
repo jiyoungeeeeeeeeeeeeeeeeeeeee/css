83cd450de921f08e09073938ad29d785411714d5
jiyoungeeeeeeeeeeeeeeeeeeeee <ghrn123123@gmail.com>
2024-07-23 06:34:54 +0900

    Update app.py

--- app.py
+++ app.py
@@ -28,24 +28,29 @@ else:
 
 
 # 탭 생성: 첫 번째 탭의 이름은 "Tab A", 두 번째 탭은 "Tab B"로 표시합니다.
-tab1, tab2 = st.tabs(['Tab A', 'Tab B'])
+tab1, tab2 = st.tabs(['Tab A', '소개합니다.'])
 
 with tab1:
     # Tab A를 누르면 표시될 내용
     st.write('Hello from Tab A!')
+    # 시연 동영상 첨부
+    video_url = "https://www.example.com/path/to/your/video.mp4"
+    st.video(video_url)
+
+
+
 
 with tab2:
     # Tab B를 누르면 표시될 내용
-    st.write('Hi from Tab B!')
+    st.write('동화책 창작에 도움을 주는 것에 대하여')
 
 
 
 
 
 
-# 시연 동영상 첨부
-video_url = "https://www.example.com/path/to/your/video.mp4"
-st.video(video_url)
+
+
 
 
 
@@ -56,21 +61,6 @@ st.write(f"입력한 이메일: {email}")
 
 
 
-
-
-
-
-# 게시글의 좋아요 수 (임의로 초기값 설정)
-likes_count = 10
-
-
-# 좋아요 버튼
-#if st.button('좋아요'):
-#    st.write('❤️ 이 글을 좋아합니다!')
-#    likes_count += 1
-#    st.write(f"게시글이 좋아요! 현재 좋아요 수: {likes_count}")
-#else:
-#    st.write(f"게시글 좋아요 수: {likes_count}")
 
 
 
