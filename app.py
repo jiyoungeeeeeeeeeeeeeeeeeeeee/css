b59075eb9d5f83e047d2e73af404363e4df7e7ae
jiyoungeeeeeeeeeeeeeeeeeeeee <ghrn123123@gmail.com>
2024-07-23 05:15:46 +0900

    update

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 import streamlit as st # type: ignore
 
-st.write("Hello, world")
+video_url = "https://www.example.com/path/to/your/video.mp4"
+st.video(video_url)
+
+
+
+email = st.text_input("이메일 입력", "example@example.com")
+st.write(f"입력한 이메일: {email}")
