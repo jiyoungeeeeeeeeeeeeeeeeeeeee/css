7be2e711f02ec200ffda1686bb25d8540b4a6d43
jiyoungeeeeeeeeeeeeeeeeeeeee <ghrn123123@gmail.com>
2024-07-23 05:39:37 +0900

    Update app.py

--- app.py
+++ app.py
@@ -13,8 +13,11 @@ st.write("안녕하세요! 이곳에 다양한 내용을 추가할 수 있습니
 
 video_url = "https://www.example.com/path/to/your/video.mp4"
 st.video(video_url)
+st.write("저희의 제품을 소개합니다.")
 
 
 
+
+st.write("저희의 제품을 구독하시려면 이메일을 입력해주세요.")
 email = st.text_input("이메일 입력", "example@example.com")
 st.write(f"입력한 이메일: {email}")
