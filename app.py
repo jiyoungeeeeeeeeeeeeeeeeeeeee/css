05ddcf8922d4ba6f63556917ff2b9ca0c6d9d946
jiyoungeeeeeeeeeeeeeeeeeeeee <ghrn123123@gmail.com>
2024-07-23 06:10:46 +0900

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.write("안녕하세요! 이곳에 다양한 내용을 추가할 수 있습니
 
 
 # 카테고리 생성
-category = st.radio('카테고리 선택', ['공포', '로맨틱', '미스테리', '고전문학'])
+category = st.selectbox('카테고리 선택', ['공포', '로맨틱', '미스테리', '고전문학'])
 
 # 선택한 카테고리에 따라 내용 표시
 if category == '공포':
@@ -49,6 +49,7 @@ st.video(video_url)
 
 
 
+# 이메일 입력
 st.write("저희의 제품을 구독하시려면 이메일을 입력해주세요.")
 email = st.text_input("이메일 입력", "example@example.com")
 st.write(f"입력한 이메일: {email}")
@@ -58,4 +59,14 @@ st.write(f"입력한 이메일: {email}")
 
 
 
+
+# 게시글의 좋아요 수 (임의로 초기값 설정)
+likes_count = 10
+
+# 좋아요 버튼
+if st.button("좋아요"):
+    likes_count += 1
+    st.write(f"게시글이 좋아요! 현재 좋아요 수: {likes_count}")
+else:
+    st.write(f"게시글 좋아요 수: {likes_count}")
 
